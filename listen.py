33efd6fd0de851d38de3cb90ff72c097238cbb40
SourceCodeEngineer <78597730+SourceCodeEngineer@users.noreply.github.com>
2021-09-28 18:36:36 +0200

    Update listen.py
    update

--- listen.py
+++ listen.py
@@ -29,5 +29,22 @@ def min(list):
 #könnte ein solcher Algorithmus aussehen? Versuchen Sie zuerst einen Algorithmus zu entwickeln,
 #bevor Sie etwas implementieren! Sie sollten insbesondere in der Lage sein, einem Bekannten zu
 #erklären, wie dieser Algorithmus funktioniert!
+import random
+
+list = []
+
+while len(list) <= 20:
+    list.append(random.randint(1,10000))
 
 #WILL BE CONTINUED!
+
+#Implementieren Sie eine Funktion add(point_a, point_b), die zwei Punkte eines karthesischen
+#Koordinatensystems addiert. Es gilt [a, b] + [x, y] = [a + x, b + y].
+
+def add(point_a, point_b):
+    new_list = []
+    new_list.append(point_a[0] + point_b[0])
+    new_list.append(point_a[1] + point_b[1])
+    return new_list
+
+

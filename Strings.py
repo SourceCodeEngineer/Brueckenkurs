cc2027d83f6b8143661ecdb6bb326525155fc5fc
SourceCodeEngineer <78597730+SourceCodeEngineer@users.noreply.github.com>
2021-09-28 19:09:15 +0200

    Create Strings.py
    Erstellung des Dokuments

---
+++ Strings.py
@@ -0,0 +1,7 @@
+#this code is written in python3 and is owned by Santino Pircher
+
+#Schreiben Sie ein Programm, mit dem zufällige Passwörter generiert werden können. Welche Zeichenklassen 
+#(Großbuchstaben, Ziffern, Sonderzeichen, . . . ) zugelassen sind, sollte konfigurierbar
+#sein. Implementieren Sie auch eine Funktion, mit der Sie beliebig viele Passwörter beliebiger
+#Länge generieren können.
+
